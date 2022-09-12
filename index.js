94a1729764c60f6ac06623799e2e1f50ace1093d
unknown <atiqit52@gmail.com>
2022-09-13 00:16:35 +0600

    nav added

--- index.js
+++ index.js
@@ -2,25 +2,76 @@
 // using jquery
 // $("h1").text("Hi ATiq")
 
-$(document).ready(function() {
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll :1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows:false,
-        dots:false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint:768,
-            setting:{
-                slidesToShow: 4
-            }
-        },{
-            breakpoint:520,
-            setting:{
-                slidesToShow:3,
-            }
-        }]
-    });
-});
+// $(document).ready(function() {
+//     $('.customer-logos').slick({
+//         slidesToShow: 3,
+//         // slidesToScroll :1,
+//         autoplay: true,
+//         autoplaySpeed: 1500,
+//         // arrows:true,
+//         dots:false,
+//         pauseOnHover: false,
+//         easing:'linear',
+//         variableWidth:true,
+//         pauseOnHover:true,
+
+//       swipeToSlide:true,
+//         responsive: [
+//             {
+//             breakpoint:768,
+//             setting:{
+//                 slidesToShow: 4,
+//             }
+//         },{
+//             breakpoint:520,
+//             setting:{
+//                 slidesToShow:2,
+//                 slidesToScroll :2,
+//             }
+//         },
+//         {
+//             breakpoint:364,
+//             setting:{
+//                 slidesToShow:1,
+//             }
+//         },
+//     ]
+//     });
+// });
+
+
+
+$('#gallery').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 8000,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    responsive: [
+                    {
+                    breakpoint:768,
+                    setting:{
+                        slidesToShow: 4,
+                    }
+                },{
+                    breakpoint:520,
+                    setting:{
+                        slidesToShow:7,
+                        slidesToScroll :2,
+                    }
+                },
+                {
+                    breakpoint:364,
+                    setting:{
+                        slidesToShow:1,
+                    }
+                },
+            ]
+  });
+
+  $(document).ready(function() {
+    $('.toggle').click(function(){
+        $('ul').toggleClass('active');
+    })  })
